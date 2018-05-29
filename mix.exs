850f81a19340f73e9f4b936fe0702478afa70829
Tomohiko Himura <eiel.hal@gmail.com>
2018-05-29 21:26:48 +0900

    refactor: umbrella-projects

--- mix.exs
+++ mix.exs
@@ -1,28 +1,11 @@
-defmodule Weekday.MixProject do
+defmodule WeekdayRoot.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :weekday,
-      version: "0.0.3",
-      elixir: "~> 1.4",
-      deps: deps(),
-      # hex
-      description: "provided day_of_week that return atom like :monday, :sunday",
-      package: %{
-        licenses: ["MIT"],
-        maintainers: [
-          "Tomohiko Himura <eiel.hal@gmail.com>"
-        ],
-        links: %{"GitHub" => "https://github.com/eiel/weekday"}
-      },
-      # Docs
-      name: "weekday",
-      source_url: "https://github.com/eiel/weekday",
-      # The main page in the docs
-      docs: [main: "Weekday", extras: ["README.md"]],
-      # coveralls
-      test_coverage: [tool: ExCoveralls]
+      apps_path: "apps",
+      start_parmanent: Mix.env() == :prod,
+      deps: deps()
     ]
   end
 
@@ -33,11 +16,6 @@ defmodule Weekday.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
-      {:inch_ex, only: :docs},
-      {:excoveralls, "~> 0.8", only: :test}
-    ]
+    []
   end
 end
